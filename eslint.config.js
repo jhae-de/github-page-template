dee63902665531bf01cdafc7d1c3a364bd5a0df4
Jan Hähne <github@jhae.de>
2025-01-15 17:16:34 +0100

    Intermediate commit

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,11 @@
 import eslint from '@eslint/js';
 import prettier from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
 import tslint from 'typescript-eslint';
 
 export default tslint.config(
   {
     extends: [eslint.configs.recommended, ...tslint.configs.recommendedTypeChecked, prettier],
     languageOptions: {
-      ecmaVersion: 2022,
-      globals: globals.node,
       parserOptions: {
         projectService: true,
       },
